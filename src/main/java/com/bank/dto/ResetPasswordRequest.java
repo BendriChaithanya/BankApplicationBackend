45eeac7f1036c08ed79ee043956424599d3692ca
BendriChaithanya <chaithanyabendri@gmail.com>
2026-07-27 16:32:55 +0530

    fixed dto getters

--- src/main/java/com/bank/dto/ResetPasswordRequest.java
+++ src/main/java/com/bank/dto/ResetPasswordRequest.java
@@ -1,5 +1,12 @@
 package com.bank.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResetPasswordRequest {
 
+    private String email;
+    private String newPassword;
 }
